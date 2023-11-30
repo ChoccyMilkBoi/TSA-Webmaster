7c33b33e2691ed13d69ced7b38e459ef325436c9
Emilio Armijo <armijoemilio08@gmail.com>
2023-11-30 12:04:35 -0600

    Trying to update scroll animations so that they ony play whilst scrolling downwards, and adding tracker to distance scrolled on the page, also refactoring some files to show what their purpose is.

---
+++ Index_app.js
@@ -0,0 +1,23 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
+window.addEventListener("scroll", setScrollVar)
+window.addEventListener("resize", setScrollVar)
+
+function setScrollVar() {
+    const htmlElement = document.documentElement
+    const percentOfScreenHeightScroll = htmlElement.scrollTop / htmlElement.clientHeight
+    console.log(Math.min (percentOfScreenHeightScroll * 100 , 100))
+    htmlElement.clientHeight
+    htmlElement.style.setProperty("--scroll", Math.min (percentOfScreenHeightScroll * 100 , 100))
+}
+setScrollVar()
